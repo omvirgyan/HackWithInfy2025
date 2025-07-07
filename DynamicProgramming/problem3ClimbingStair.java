281f920a5bbb70c24560c4f3b8024770208f2d9c
omvirgyan <omvirgyan@gmail.com>
2025-07-07 13:17:25 +0530

    dp

--- DynamicProgramming/problem3ClimbingStair.java
+++ DynamicProgramming/problem3ClimbingStair.java
@@ -1,9 +1,11 @@
 public class problem3ClimbingStair {
-    public static int climb(int i){
-        int num=1;
-    
+    public static int climb(int n) {
+        if (n == 0 || n == 1) return 1;
+        return climb(n - 1) + climb(n - 2);
     }
+
     public static void main(String[] args) {
-      
+        int n = 5; 
+        System.out.println(climb(n)); 
     }
 }
--- DynamicProgramming/problem4_CoinChange.java
+++ DynamicProgramming/problem4_CoinChange.java
@@ -1,18 +1,23 @@
+import java.util.Arrays;
+
 public class problem4_CoinChange {
-    public static int minCoin(int[] arr,int amount){
-        int[] dp=new int[amount+1];
-        
-        for(int i=0;i<arr.length;i++){
-            for(int j=1;j<=amount;j++){
-                 if(arr[i]-j==0) dp[j]=1;
-                 else{
-                    
-                 }
+    public static int minCoin(int[] arr, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1); 
+        dp[0] = 0; 
+
+        for (int coin : arr) {
+            for (int j = coin; j <= amount; j++) {
+                dp[j] = Math.min(dp[j], dp[j - coin] + 1);
             }
         }
 
-    } 
-    public static void main(String[] args) {
-        
+        return dp[amount] > amount ? -1 : dp[amount];
     }
+
+    public static void main(String[] args) {
+        int[] coins = {1, 2, 5};
+        int amount = 11;
+        System.out.println(minCoin(coins, amount)); 
+}
 }
