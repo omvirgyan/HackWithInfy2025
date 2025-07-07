9c13c5c1917fecb2671b86d683d642139347be76
omvirgyan <omvirgyan@gmail.com>
2025-07-07 11:35:10 +0530

    greedy approach

---
+++ GreedyApproach/problem6_Buy_Two_Chocolates.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+public class problem6_Buy_Two_Chocolates {
+    
+    public static int buyChoco(int[] prices, int money) {
+        Arrays.sort(prices);
+        int cost = prices[0] + prices[1];
+
+        if (money >= cost) {
+            return money - cost;
+        } else {
+            return money;
+        }
+    }    
+    
+     public static void main(String[] args) {
+        
+        int[] prices = {1, 2, 2};
+        int money = 5;
+
+        int remaining = buyChoco(prices, money);
+        System.out.println("Remaining money: " + remaining);
+    }
+}
